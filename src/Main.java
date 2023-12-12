614fa478c6157e4bf4274194276cb33e676d9e03
cinoxil <c.ugurongun@gmail.com>
2023-12-12 13:58:43 +0300

    changed variable names

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int kenar1,kenar2,kenar3;
-        double cevre,alan;
+        int kenar1, kenar2, kenar3;
+        double u, alan;
         System.out.print("Lütfen 1.kenarı girin : ");
         kenar1 = inp.nextInt();
 
@@ -13,8 +14,8 @@ public class Main {
         System.out.print("Lütfen 3.kenarı girin : ");
         kenar3 = inp.nextInt();
 
-        cevre = (kenar1+kenar2+kenar3)/2;
-        alan = Math.sqrt(cevre*(cevre-kenar1)*(cevre-kenar2)*(cevre-kenar3));
+        u = (kenar1 + kenar2 + kenar3) / 2;
+        alan = Math.sqrt(u * (u - kenar1) * (u - kenar2) * (u - kenar3));
 
         System.out.print("Üçgenin alanı : " + alan);
     }
